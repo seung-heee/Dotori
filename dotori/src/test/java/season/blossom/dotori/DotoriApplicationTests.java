1ef1e1bd49e087c9e93dd1f535d20841da2d08e5
guitarcode <choicco89@gmail.com>
2023-02-13 23:45:44 +0900

    feat : 스프링부트 프로젝트 생성

---
+++ dotori/src/test/java/season/blossom/dotori/DotoriApplicationTests.java
@@ -0,0 +1,13 @@
+package season.blossom.dotori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
